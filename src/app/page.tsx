8b2348abf04bf6f2bb132d94724b0dd1c12794c3
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-17 15:05:15 +0700

    Update Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,92 @@
-export default function Home() {
-  return <div>Home Page</div>;
+"use client";
+import React, {useEffect, useState} from "react";
+import Wrapper from "@/components/Wrapper";
+import HeroBanner from "@/components/HeroBanner";
+import BrandBanner from "@/components/BrandBanner";
+import Category from "@/components/Category";
+// import LoadingCard from "@/components/card/LoadingCard";
+// import ProductCard from "@/components/card/ProductCard";
+
+import {getdata, listby} from "@/functions/product";
+
+export default function page() {
+  const [product, setProduct] = useState([]);
+  const [productTop, setProductTop] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadData();
+    loadTopSellData();
+  }, []);
+
+  const loadData = async () => {
+    await listby("createdAt", "desc")
+      .then((res) => {
+        // console.log(res.data);
+        setProduct(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  const loadTopSellData = async () => {
+    await listby("sale_amount", "desc")
+      .then((res) => {
+        console.log(res.data);
+        setProductTop(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <main>
+      <HeroBanner />
+      <BrandBanner />
+      <Wrapper>
+        <h1 className="flex justify-center text-5xl font-black pt-20">
+          NEW ARRIVALS
+        </h1>
+
+        {/* products grid start */}
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pt-10 max-w-[1280px] mx-auto p-20 lg:p-0">
+          {loading ? (
+            <LoadingCard count={3} />
+          ) : (
+            // Loop items
+            product.slice(0, 8).map((item) => <ProductCard data={item} />)
+          )}
+        </div> */}
+        {/* products grid end */}
+
+        <h1
+          id="top-selling"
+          className="flex justify-center text-5xl font-black pt-20"
+        >
+          TOP SELLING
+        </h1>
+        {/* products grid start */}
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pt-10 max-w-[1280px] mx-auto p-20 lg:p-0 ">
+          {loading ? (
+            <LoadingCard count={3} />
+          ) : (
+            // Loop items
+            productTop.slice(0, 12).map((item) => <ProductCard data={item} />)
+          )}
+        </div> */}
+        {/* products grid end */}
+
+        <div id="category">
+          <Category />
+        </div>
+      </Wrapper>
+      {/* <Testimonial /> */}
+    </main>
+  );
 }
